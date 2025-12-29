6ee477ebc9dc90381ec35ada07330ae3878fcd59
devangnpm <d3vang2002@gmail.com>
2025-12-29 21:41:26 +0530

    terraform security groups add

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,22 +2,49 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "tg-bot" {
-  ami           = data.aws_ami.ubuntu.id
+// new aws instance
+resource "aws_instance" "bot_instance" {
+  ami           = "ami-02b8269d5e85954ef" // amazon ami image id (ubuntu 64 bit architecture)
   instance_type = "t2.micro"
+  # user_data = <- EOF
 
+  // dashboard instance name
   tags = {
     Name = "tg-bot"
   }
+}
+
+// defining our security group here for allowed traffic on our server
+resource "aws_security_group" "bot_firewall" {
+  name        = "bot-firewall"
+  description = "Allow SSH and Web traffic"
+  vpc_id      = aws_vpc.main.id
+}
+
+// open ssh port
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.bot_firewall.id
+
+  # ssh allowed only from my ip
+  cidr_ipv4   = "${var.my_ip}/32"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+// inbound rule for all ipv4
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.bot_firewall.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# outbound Rule allow all outbound traffic
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.bot_firewall.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # -1 means "all protocols"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "my_ip" {
+  description = "My pc public IP address"
+  type        = string
+  sensitive   = true
+}
